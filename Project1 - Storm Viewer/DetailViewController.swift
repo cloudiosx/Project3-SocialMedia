7843cb5e3b89041429c43a0a94ae35fab8228216
John Kim <cloudiosx@gmail.com>
2022-01-23 20:34:48 -0500

    Add UIActivityViewController in the DetailViewController and shareTapped method

--- Project1 - Storm Viewer/DetailViewController.swift
+++ Project1 - Storm Viewer/DetailViewController.swift
@@ -22,6 +22,10 @@ class DetailViewController: UIViewController {
         
         navigationItem.largeTitleDisplayMode = .never
         
+        // UIActivityViewController
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         // Loading images with UIImage
         
         if let imageToLoad = selectedImage {
@@ -39,6 +43,17 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
 
     /*
